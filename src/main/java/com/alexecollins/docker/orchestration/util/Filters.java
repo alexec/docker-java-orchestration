8afac73c82caa4451bec5a0707b8d18ca248cb9e
Alex Collins <alex.e.c@gmail.com>
2014-05-24 18:55:58 +0100

    filtered

---
+++ src/main/java/com/alexecollins/docker/orchestration/util/Filters.java
@@ -0,0 +1,87 @@
+package com.alexecollins.docker.orchestration.util;
+
+
+import java.io.*;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Properties;
+import java.util.TreeSet;
+
+public final class Filters {
+	private Filters() {
+	}
+
+	public static void filter(File file, Properties properties) throws IOException {
+		filter(file, new FileFilter() {
+			@Override
+			public boolean accept(File pathname) {
+				return true;
+			}
+		}, properties);
+	}
+
+	public static void filter(File file, FileFilter fileFilter, Properties properties) throws IOException {
+
+		if (file == null) {
+			throw new IllegalArgumentException("file is null");
+		}
+		if (!file.exists()) {
+			throw new IllegalArgumentException("file " + file + " does not exist");
+		}
+		if (fileFilter == null) {
+			throw new IllegalArgumentException("fileFilter is null");
+		}
+		if (properties == null) {
+			throw new IllegalArgumentException("properties is null");
+		}
+
+		if (file.isDirectory()) {
+			//noinspection ConstantConditions
+			for (File child : file.listFiles()) {
+				filter(child, fileFilter, properties);
+			}
+		} else if (fileFilter.accept(file)) {
+			final File outFile = new File(file + ".tmp");
+			final BufferedReader in = new BufferedReader(new FileReader(file));
+			try {
+				final PrintWriter out = new PrintWriter(new FileWriter(outFile));
+				try {
+					String l;
+					while ((l = in.readLine()) != null) {
+						// ${...}
+						if (l.matches(".*\\$\\{.*\\}.*")) {
+							for (Map.Entry<Object, Object> e : properties.entrySet()) {
+								l = l.replace("${" + e.getKey() + "}", e.getValue().toString());
+							}
+						}
+						out.println(l);
+					}
+				} finally {
+					out.close();
+				}
+			} finally {
+				in.close();
+			}
+
+			move(outFile, file);
+		}
+	}
+
+	private static void move(File from, File to) throws IOException {
+		if (!from.renameTo(to)) {
+			throw new IOException("failed to move " + from + " to " + to);
+		}
+	}
+
+	static int maxKeyLength(Properties properties) {
+		final TreeSet<Object> t = new TreeSet<Object>(new Comparator<Object>() {
+
+			@Override
+			public int compare(Object o1, Object o2) {
+				return o1.toString().compareTo(o2.toString());
+			}
+		});
+		t.addAll(properties.keySet());
+		return t.last().toString().length();
+	}
+}
---
+++ src/test/java/com/alexecollins/docker/orchestration/util/FiltersTest.java
@@ -0,0 +1,48 @@
+package com.alexecollins.docker.orchestration.util;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Properties;
+import java.util.Scanner;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+public class FiltersTest {
+
+	private File dir;
+	private File file;
+
+	@Before
+	public void setUp() throws Exception {
+		dir = new File("target/test/filters");
+		assert dir.mkdirs();
+
+		file = new File(dir, "test.txt");
+		new FileOutputStream(file).write("test ${project.version}\n".getBytes());
+	}
+
+	@Test
+	public void testFilter() throws Exception {
+		final Properties properties = new Properties();
+		properties.setProperty("project.version", "1.0.0");
+
+		Filters.filter(dir, properties);
+
+		assertThat(new Scanner(file).useDelimiter("\\A").next(), equalTo("test 1.0.0\n"));
+	}
+
+	@Test
+	public void testMaxLength() throws Exception {
+		final Properties p = new Properties();
+		p.setProperty("a", "1");
+		p.setProperty("aa", "1");
+
+		assertEquals(2, Filters.maxKeyLength(p));
+
+	}
+}
