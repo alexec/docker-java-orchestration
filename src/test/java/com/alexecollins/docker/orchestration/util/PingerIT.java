c3ce90de2d505ca5cf8d51f27a2c96521cd0104b
Alex Collins <alex.e.c@gmail.com>
2014-10-26 13:38:00 +0000

    converted test to IT so it can run offline

---
+++ src/test/java/com/alexecollins/docker/orchestration/util/PingerIT.java
@@ -0,0 +1,21 @@
+package com.alexecollins.docker.orchestration.util;
+
+import org.junit.Test;
+
+import java.net.URI;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class PingerIT {
+
+    @Test
+    public void testGoogle() throws Exception {
+        assertTrue(Pinger.ping(URI.create("http://www.google.com")));
+    }
+
+    @Test
+    public void testNoop() throws Exception {
+        assertFalse(Pinger.ping(URI.create("http://noop")));
+    }
+}
