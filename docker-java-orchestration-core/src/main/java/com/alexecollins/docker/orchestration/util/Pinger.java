3792a897411f848e65a8085be6511e60ffaf9a37
Alex Collins <alex.e.c@gmail.com>
2015-01-17 21:34:41 +0000

    expanded pinger so that it can test body of response for regexp

--- docker-java-orchestration-core/src/main/java/com/alexecollins/docker/orchestration/util/Pinger.java
+++ docker-java-orchestration-core/src/main/java/com/alexecollins/docker/orchestration/util/Pinger.java
@@ -1,8 +1,11 @@
 package com.alexecollins.docker.orchestration.util;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URI;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 import static java.lang.System.currentTimeMillis;
 
@@ -10,28 +13,33 @@ public final class Pinger {
 	private Pinger() {
 	}
 
-	public static boolean ping(URI uri, int timeout) {
+	public static boolean ping(URI uri, Pattern pattern, int timeout) {
 		long start = currentTimeMillis();
-		while (!ping(uri)) {
+		while (!ping(uri, pattern)) {
 			if (currentTimeMillis() - start > timeout) {
 				return false;
 			}
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+		}
 		return true;
 	}
 
-	public static boolean ping(URI uri) {
+	public static boolean ping(URI uri, int timeout) {
+		return ping(uri,null,timeout);
+	}
+
+	public static boolean ping(URI uri, Pattern pattern) {
 		try {
 			final HttpURLConnection c = (HttpURLConnection) uri.toURL().openConnection();
 			c.setRequestProperty("Accept", "*/*");
 			c.connect();
 			try {
-				return c.getResponseCode() == 200;
+				return c.getResponseCode() == 200 &&
+						(pattern == null || pattern.matcher(readAll(c.getInputStream())).find());
 			} finally {
 				c.disconnect();
 			}
@@ -39,4 +47,9 @@ public final class Pinger {
 			return false;
 		}
 	}
+
+	private static String readAll(InputStream inputStream) {
+		return new Scanner(inputStream).useDelimiter("\\A").next();
+	}
+
 }
--- docker-java-orchestration-core/src/test/java/com/alexecollins/docker/orchestration/util/PingerIT.java
+++ docker-java-orchestration-core/src/test/java/com/alexecollins/docker/orchestration/util/PingerIT.java
@@ -3,6 +3,7 @@ package com.alexecollins.docker.orchestration.util;
 import org.junit.Test;
 
 import java.net.URI;
+import java.util.regex.Pattern;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -11,11 +12,21 @@ public class PingerIT {
 
     @Test
     public void testGoogle() throws Exception {
-        assertTrue(Pinger.ping(URI.create("http://www.google.com")));
+        assertTrue(Pinger.ping(URI.create("http://www.google.com"), 5000));
+    }
+
+    @Test
+    public void ensureRegexpMatches() throws Exception {
+        assertTrue(Pinger.ping(URI.create("http://www.alexecollins.com"), Pattern.compile("Alex Collins"), 1000));
+    }
+
+    @Test
+    public void ensureBadRegexpDoesNotMatch() throws Exception {
+        assertFalse(Pinger.ping(URI.create("http://www.alexecollins.com"), Pattern.compile("Bill Murray"), 1000));
     }
 
     @Test
     public void testNoop() throws Exception {
-        assertFalse(Pinger.ping(URI.create("http://noop")));
+        assertFalse(Pinger.ping(URI.create("http://noop"), 5000));
     }
 }
