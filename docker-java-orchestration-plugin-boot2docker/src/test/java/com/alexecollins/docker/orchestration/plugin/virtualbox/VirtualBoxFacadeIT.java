fe84793a9ece435478fdec84210271d2e45639d4
alex.collins <alex.collins@gamesys.co.uk>
2015-04-30 19:26:29 +0100

    Extracted VirtualBoxFacade from Boot2DockerPlugin to try and make it more robust with Windows.

--- docker-java-orchestration-plugin-boot2docker/src/test/java/com/alexecollins/docker/orchestration/plugin/virtualbox/VirtualBoxFacadeIT.java
+++ docker-java-orchestration-plugin-boot2docker/src/test/java/com/alexecollins/docker/orchestration/plugin/virtualbox/VirtualBoxFacadeIT.java
@@ -13,7 +13,7 @@ public class VirtualBoxFacadeIT {
 
     @After
     public void tearDown() throws Exception {
-        if (OS.isUnix()) {
+        if (!OS.isUnix()) {
             virtualBoxFacade.deletePortForward(PORT);
         }
     }
@@ -21,7 +21,7 @@ public class VirtualBoxFacadeIT {
     @Test
     public void canCreateAndDeletePortForward() throws Exception {
 
-        if (OS.isUnix()) {
+        if (!OS.isUnix()) {
             virtualBoxFacade.createPortForward(PORT);
             assertTrue(virtualBoxFacade.getPortForwards().contains(PORT));
             virtualBoxFacade.recreatePortForward(PORT);
