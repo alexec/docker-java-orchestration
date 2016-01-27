e4ef2016479fdb8b5807e510284642914b13b949
Marcus Thiesen <mt@hubrick.com>
2016-01-27 12:08:09 +0100

    Port to docker-java 3.0.0

--- docker-java-orchestration-core/src/test/java/com/alexecollins/docker/orchestration/DockerOrchestratorTest.java
+++ docker-java-orchestration-core/src/test/java/com/alexecollins/docker/orchestration/DockerOrchestratorTest.java
@@ -11,9 +11,24 @@ import com.alexecollins.docker.orchestration.model.LogPattern;
 import com.alexecollins.docker.orchestration.model.VolumeFrom;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.dockerjava.api.DockerClient;
-import com.github.dockerjava.api.DockerException;
 import com.github.dockerjava.api.async.ResultCallback;
-import com.github.dockerjava.api.command.*;
+import com.github.dockerjava.api.command.BuildImageCmd;
+import com.github.dockerjava.api.command.CopyFileFromContainerCmd;
+import com.github.dockerjava.api.command.CreateContainerCmd;
+import com.github.dockerjava.api.command.CreateContainerResponse;
+import com.github.dockerjava.api.command.InspectContainerCmd;
+import com.github.dockerjava.api.command.InspectContainerResponse;
+import com.github.dockerjava.api.command.ListContainersCmd;
+import com.github.dockerjava.api.command.ListImagesCmd;
+import com.github.dockerjava.api.command.LogContainerCmd;
+import com.github.dockerjava.api.command.PushImageCmd;
+import com.github.dockerjava.api.command.RemoveContainerCmd;
+import com.github.dockerjava.api.command.RemoveImageCmd;
+import com.github.dockerjava.api.command.SaveImageCmd;
+import com.github.dockerjava.api.command.StartContainerCmd;
+import com.github.dockerjava.api.command.StopContainerCmd;
+import com.github.dockerjava.api.command.TagImageCmd;
+import com.github.dockerjava.api.exception.DockerException;
 import com.github.dockerjava.api.model.AuthConfig;
 import com.github.dockerjava.api.model.BuildResponseItem;
 import com.github.dockerjava.api.model.Container;
@@ -21,6 +36,7 @@ import com.github.dockerjava.api.model.ContainerConfig;
 import com.github.dockerjava.api.model.Frame;
 import com.github.dockerjava.api.model.Identifier;
 import com.github.dockerjava.api.model.Image;
+import com.github.dockerjava.api.model.NetworkSettings;
 import com.github.dockerjava.api.model.PushResponseItem;
 import com.github.dockerjava.api.model.Repository;
 import com.github.dockerjava.api.model.StreamType;
@@ -111,7 +127,7 @@ public class DockerOrchestratorTest {
     @Mock
     private InspectContainerResponse containerInspectResponseMock;
     @Mock
-    private InspectContainerResponse.NetworkSettings networkSettingsMock;
+    private NetworkSettings networkSettingsMock;
     @Mock
     private BuildImageCmd buildImageCmdMock;
     @Mock
@@ -245,7 +261,7 @@ public class DockerOrchestratorTest {
         when(dockerMock.stopContainerCmd(CONTAINER_ID)).thenReturn(stopContainerCmdMock);
         when(dockerMock.removeContainerCmd(CONTAINER_ID)).thenReturn(removeContainerCmdMock);
         when(dockerMock.listImagesCmd()).thenReturn(listImagesCmdMock);
-        when(removeContainerCmdMock.withForce()).thenReturn(removeContainerCmdMock);
+        when(removeContainerCmdMock.withForce(true)).thenReturn(removeContainerCmdMock);
         when(removeContainerCmdMock.withRemoveVolumes(true)).thenReturn(removeContainerCmdMock);
 
         when(listImagesCmdMock.exec()).thenReturn(Collections.singletonList(imageMock));
@@ -622,11 +638,11 @@ public class DockerOrchestratorTest {
     private LogContainerCmd mockLogContainerCmd(final String containerOutput) {
         final LogContainerCmd cmd = mock(LogContainerCmd.class);
 
-        when(cmd.withStdErr()).thenReturn(cmd);
-        when(cmd.withStdOut()).thenReturn(cmd);
+        when(cmd.withStdErr(true)).thenReturn(cmd);
+        when(cmd.withStdOut(true)).thenReturn(cmd);
         when(cmd.withTailAll()).thenReturn(cmd);
-        when(cmd.withFollowStream()).thenReturn(cmd);
-        when(cmd.withTimestamps()).thenReturn(cmd);
+        when(cmd.withFollowStream(true)).thenReturn(cmd);
+        when(cmd.withTimestamps(true)).thenReturn(cmd);
 
         when(cmd.exec(any(ResultCallback.class))).thenAnswer(
                 new Answer<Object>() {
