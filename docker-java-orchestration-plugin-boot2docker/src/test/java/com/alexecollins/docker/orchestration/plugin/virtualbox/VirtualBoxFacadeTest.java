c5d222bfae596bb2ba02b32cd4d62ae79ef09483
Christopher Eagan <ceagan@gmail.com>
2016-07-20 20:26:21 -0400

    Improved Image ID Identification Selection.
    * Resolves an issue where Docker images are incorrectly matched when two images exist and the name of one forms the prefix of the other.
    * Updated unit test mock objects to better reflect the values seen at runtime.
    * Resolved an issue in VirtualBoxFacadeTest where the code was expecting a System.getProperty("line.separator") rather than a "\n".

--- docker-java-orchestration-plugin-boot2docker/src/test/java/com/alexecollins/docker/orchestration/plugin/virtualbox/VirtualBoxFacadeTest.java
+++ docker-java-orchestration-plugin-boot2docker/src/test/java/com/alexecollins/docker/orchestration/plugin/virtualbox/VirtualBoxFacadeTest.java
@@ -18,7 +18,8 @@ public class VirtualBoxFacadeTest {
     public void canParseVmNames() throws Exception {
 
         when(commandExecutor.exec(anyString())).thenReturn(
-                "\"db2-972-exc-5_default_1433864805042_17400\" {e7fbeef6-f9f9-4c5a-a57a-9571c1fe7e67}\n" +
+                "\"db2-972-exc-5_default_1433864805042_17400\" {e7fbeef6-f9f9-4c5a-a57a-9571c1fe7e67}" +
+                        System.getProperty("line.separator") +
                         "\"default\" {90d48200-4395-4346-83af-8cbd537e7009}"
         );
 
